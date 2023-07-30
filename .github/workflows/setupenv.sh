7fa64b058d9794ec2a8335baa4e1c81e0670d82c
Chinmaya Sahoo <sahoo.c@partner.samsung.com>
2023-07-30 13:40:35 -0500

    updated

--- .github/workflows/setupenv.sh
+++ .github/workflows/setupenv.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-GIT_SHA=`echo -n ${{github.sha}}`
-echo ::set-output name=GIT_SHA::${GIT_SHA}  ## Git latest SHA hash
 echo "myname=chinmaya" >> $GITHUB_OUTPUT
 echo "GIT_SHA=${GIT_SHA}" >> $GITHUB_OUTPUT
