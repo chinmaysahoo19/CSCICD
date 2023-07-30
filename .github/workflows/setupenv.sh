389ab6e82cfd519ba5c5c52e644da91d27a23a32
Chinmaya Sahoo <sahoo.c@partner.samsung.com>
2023-07-30 14:01:54 -0500

    updated

--- .github/workflows/setupenv.sh
+++ .github/workflows/setupenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "MYKEY IS  - ${STAGE_API_KEY}"
 
-echo "MYKEY IS  SECRECT  - ${{ secrets.STAGE_API_KEY }}"
+echo "MYKEY IS  SECRECT  - ${SECRECT_STAGE_API_KEY}"
 
 echo "myname=chinmaya" >> $GITHUB_OUTPUT
 echo "GIT_SHA=${GIT_SHA}" >> $GITHUB_OUTPUT
