873205ed60fd6bfb2e4c5e0387467a370ea48bff
Chinmaya Sahoo <sahoo.c@partner.samsung.com>
2023-08-08 10:03:45 -0500

    updated

--- .github/workflows/git-utils.sh
+++ .github/workflows/git-utils.sh
@@ -8,7 +8,7 @@ function configure_pk() {
 }
 
 function git_configure() {
-  #configure_pk
+  configure_pk
   #git config --global --add safe.directory "${master_PROJECT_DIR}" &&
     git config --global user.name "$GITHUB_ACTOR"
     #git config --global user.email "$GITLAB_USER_EMAIL" &&
